e5de73fc29e383f5eb1317669c366b8a15a4d6f4
kalin chakma <chakma.kalin10211@gmail.com>
2025-01-20 03:04:04 +0600

    added env pagage

--- gs_go/cmd/api/main.go
+++ gs_go/cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/kalinchkma/playgrround/gs_go/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 	app := &application{
 		config: cfg,
