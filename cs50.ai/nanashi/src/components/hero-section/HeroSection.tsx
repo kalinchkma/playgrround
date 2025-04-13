9741a1a0f7147e529be4c168ba53e1dc67d102d8
kalin chakma <chakma.kalin10211@gmail.com>
2025-04-13 15:21:35 +0600

    updated hero section design

--- cs50.ai/nanashi/src/components/hero-section/HeroSection.tsx
+++ cs50.ai/nanashi/src/components/hero-section/HeroSection.tsx
@@ -3,8 +3,9 @@ import {motion} from "framer-motion";
 
 // import Link from "next/link"
 // import { Icons } from "../common/icons"
-import Image from "next/image"
+
 import { Icons } from "../common/icons";
+import { Avatar, AvatarImage } from "../ui/avatar";
 
 function HeroSection() {
   return (
@@ -12,24 +13,20 @@ function HeroSection() {
     initial={{ opacity: 0, y: 20 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.5 }}
-    className="text-center flex flex-col w-full items-center justify-center"
+    className="text-center  w-full md:max-w-3xl mx-auto "
   >    
     <div className="flex flex-row w-full items-center justify-center gap-5">
         {/* logo container */}
-        <div className="flex items-center justify-center border p-1 rounded-full overflow-hidden w-80 h-80">
-            <Image
-                src={'/mrk.png'} 
-                width={300} 
-                height={300}
-                alt="kalin chakma" 
-                className="w-full h-full object-cover" 
-            />
+        <div className="flex grow">
+            <Avatar className="w-[350px] h-[350px]">
+                <AvatarImage src="mrk.png" />
+            </Avatar>
         </div>
-        <div className="flex flex-col items-start justify-start">
+        <div className="flex flex-col items-start justify-start gap-6">
             {/* description section */}
             <div className="text-zinc-100">
                 <h1 className="text-4xl font-bold mb-4 text-left">Hello, I&apos;m Kalin Chakma</h1>
-                <p className="text-lg max-w-xl mb-6 text-left">
+                <p className="text-lg max-w-xl text-left">
                 Software Engineer | Passionate about making electronic circuits into intelligence being
                 </p>
             </div>
@@ -40,28 +37,28 @@ function HeroSection() {
                 I am just a human species with complex molecules and other energy or particle simulations stuck on a binary system in augmented reality
                 </p>
             </div>
-        </div>
-    </div>
-    <div className=" flex flex-col md:flex-row gap-12 text-zinc-100">
-        {/* Email */}
-        <div className='flex flex-col items-start'>
-            <p className="font-semibold mb-1 text-xl">Email</p>
-            <p>info@mysite.com</p>
-        </div>
-
-        {/* Socials */}
-        <div className='flex flex-col items-start'>
-            <p className="font-semibold mb-1 text-xl">Social</p>
-            <div className="flex gap-3 mt-1">
-                <a href="https://linkedin.com" target="_blank" className="hover:text-black">
-                <Icons.gitHub className='w-5 h-5' />
-                </a>
-                <a href="https://twitter.com" target="_blank" className="hover:text-black">
-                <Icons.x className='w-5 h-5'/>
-                </a>
+            <div className="w-full flex flex-col md:flex-row gap-12 text-zinc-100">
+                {/* Email */}
+                <div className='flex flex-col items-start'>
+                    <p className="font-semibold mb-1 text-xl">Email</p>
+                    <p>info@mysite.com</p>
+                </div>
+                {/* Socials */}
+                <div className='flex flex-col items-start'>
+                    <p className="font-semibold mb-1 text-xl">Connect me</p>
+                    <div className="flex gap-3 mt-1 items-center">
+                        <a href="https://linkedin.com" target="_blank" className="hover:text-black">
+                        <Icons.gitHub className='w-6 h-6' />
+                        </a>
+                        <a href="https://twitter.com" target="_blank" className="hover:text-black">
+                        <Icons.x className='w-5 h-5'/>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
   </motion.div>
   )
 }
