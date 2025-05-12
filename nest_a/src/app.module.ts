0c6753cc807ecd76b81a87fd43498e3f7ed8cae4
kalin chakma <chakma.kalin10211@gmail.com>
2025-05-13 01:10:21 +0600

    working with langchain

--- nest_a/src/app.module.ts
+++ nest_a/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { LangchainModule } from './modules/langchain/langchain.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, LangchainModule],
   controllers: [AppController],
   providers: [AppService],
 })
