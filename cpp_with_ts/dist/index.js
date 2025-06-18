2728042f2626fdd4086d3d5631434fc695739b61
Kalin Chakma <mrk@Kalins-Mac-mini.local>
2025-06-19 00:03:02 +0600

    remove generated file form git

--- cpp_with_ts/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const addon_node_1 = __importDefault(require("./build/Release/addon.node"));
-console.log(addon_node_1.default.myMethod()); // Outputs: Hello from C++!
